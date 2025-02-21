7761ebcdc37cc03414c03afe929d507dee463724
i3artek <bartekwzorek@onet.pl>
2025-02-21 16:20:41 +0100

    Initial Commit

---
+++ ComputerGraphics_1/ComputerGraphics_1App.swift
@@ -0,0 +1,17 @@
+//
+//  ComputerGraphics_1App.swift
+//  ComputerGraphics_1
+//
+//  Created by I3artek on 21/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct ComputerGraphics_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
